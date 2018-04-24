8dd59d5531a49ff3745a4249d2fdb5c086314636
liuziyang <616560426@qq.com>
2018-04-24 22:22:48 +0800

    commit

--- computing-core/src/main/scala/com/ml/kaggle/MedicalCalculate.scala
+++ computing-core/src/main/scala/com/ml/kaggle/MedicalCalculate.scala
@@ -1,6 +1,6 @@
 package com.ml.kaggle
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
   * Created by Administrator on 2018/4/24.
@@ -20,19 +20,26 @@ object MedicalCalculate{
     val data2_path="data_part2.txt"
     val train_path="train.csv"
     val test_path="test.csv"
-    val data_df=medicalCalculate.getDataDF(data1_path)
-    data_df.show(false)
+    val data1_df=medicalCalculate.getDataDF(data2_path,"$")
+    val data2_df=medicalCalculate.getDataDF(data2_path,"$")
 
+    val data_df=data1_df.join(data2_df,Seq("vid"),)
+    println(data_df.count())
+    println(data1_df.count())
+    print(data2_df.count())
+//    data1_df.show(false)
+data_df.printSchema()
   }
 
 }
 class MedicalCalculate(spark:SparkSession) {
 import spark.implicits._
   val base_path="E:\\dataset\\medicalCalculate\\20180408\\"
-def getDataDF(path:String)={
+def getDataDF(path:String,sep:String): DataFrame ={
   spark.read.option("header","true")
     .option("nullValue","NA")
     .option("inferSchema","true")
+      .option("sep",sep)
 .csv(base_path+path)
 
 }
