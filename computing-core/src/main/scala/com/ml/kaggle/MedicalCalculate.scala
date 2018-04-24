0302aa284890007a08f55e0f997167a7e66403ab
liuziyang <616560426@qq.com>
2018-04-24 22:10:32 +0800

    commit

---
+++ computing-core/src/main/scala/com/ml/kaggle/MedicalCalculate.scala
@@ -0,0 +1,41 @@
+package com.ml.kaggle
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by Administrator on 2018/4/24.
+  */
+
+
+object MedicalCalculate{
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("medical")
+      .getOrCreate()
+    val sc=spark.sparkContext.setLogLevel("warn")
+
+    val medicalCalculate=new MedicalCalculate(spark)
+    val data1_path="data_part1.txt"
+    val data2_path="data_part2.txt"
+    val train_path="train.csv"
+    val test_path="test.csv"
+
+
+  }
+
+}
+class MedicalCalculate(spark:SparkSession) {
+import spark.implicits._
+  val base_path="E:\\dataset\\medicalCalculate\\20180408\\"
+def getDataDF(path:String)={
+  spark.read.option("header","true")
+    .option("nullValue","NA")
+    .option("inferSchema","true")
+
+
+}
+
+
+
+}
